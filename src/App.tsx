629047afc455d5803cb54869922b31bfab259868
xiancao2024 <cao.xian@northeastern.edu>
2024-09-17 00:13:52 -0700

    update after the first week: assignment 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Labs from "./Labs";
 
 function App() {
   return (
-    <div className="Xian Cao's App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to web app builded by Xian Cao 2024 fall!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome to Web Dev!!</h1>
+      <Labs />
     </div>
   );
 }
