47e52b912f53c701188a274d520dd2a7096777da
xiancao2024 <cao.xian@northeastern.edu>
2024-09-19 19:46:53 -0700

    final version

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 export default function LandingPage() {
     return (
         <div id="landing-page">
+
             {/* Display your name and course section */}
             <h1>Xian Cao</h1>
             <h3>Section: CS5610-FALL24</h3>
@@ -26,7 +27,7 @@ export default function LandingPage() {
             {/* Source Code Repositories */}
             <h2>Source Code Repositories</h2>
             <ul>
-                <li><a href="https://github.com/inspiritU/kanbas-react-web-app-cs5610-fa24/tree/a1" target="_blank">A1 Repo</a></li>
+                <li><a href="https://github.com/xiancao2024/kanbas-react-web-app-cs5610-fa24" target="_blank">A1 Repo</a></li>
             </ul>
         </div>
     );
