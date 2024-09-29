94b32ed01df36f854b510c17f78fe72fb9a31e97
xiancao2024 <cao.xian@northeastern.edu>
2024-09-29 15:28:07 -0700

    update lab link

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -33,7 +33,7 @@ export default function TOC() {
         </a>
       </li>
       <li className="nav-item">
-        <a id="wd-k" href="https://github.com/jannunzi" className="nav-link">
+        <a id="wd-k" href="https://github.com/xiancao2024" className="nav-link">
           My GitHub
         </a>
       </li>
